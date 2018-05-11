1bf870758e602d06397feecc2926bc3aca6eca44
Goldscirer <seweryn.wawrzynowicz@gmail.com>
2018-05-11 09:45:00 +0200

    Dla Kacperka <3

--- app/src/main/java/com/example/seweryn/dronapplication/ActivityConfigurationDron.java
+++ app/src/main/java/com/example/seweryn/dronapplication/ActivityConfigurationDron.java
@@ -2,21 +2,21 @@ package com.example.seweryn.dronapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ActivityConfigurationDron extends AppCompatActivity {
 
+    ConfigurationMessage message;
     MessageConnectionHandler connectionHandler = new MessageConnectionHandler(this, "");
-    TextView configurationText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration_dron);
-
-        configurationText = (TextView)findViewById(R.id.configurationText);
 
     }
 
@@ -29,13 +29,21 @@ public class ActivityConfigurationDron extends AppCompatActivity {
             MqttConnection conn = new MqttConnection(this);
             if(conn.Connect() == true)
             {
-                //configurationText.setText(MessageConnectionHandler.CONF_TEXT);
+                Log.d("tag", "Connection and sub works");
             }
         }
         else
         {
             Toast.makeText( this, "You must set up ip address!", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void uploadDownloadConfiguration(View v) {
+        ActivityConnect.TOPIC_NAME = Topics.UPDATE;
+        Log.d("tag", "TO jest update => " + MessageConnectionHandler.JSON.Rotor_1_1);
+
 
     }
+
+
 }
